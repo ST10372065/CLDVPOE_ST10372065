b4a6c6f1a7b29222701bff21dcdc2100b75edb82
Zack Stangroom <st10372065@vcconnect.edu.za>
2024-04-30 14:24:46 +0200

    renamed file from tester to userTable and added connection

---
+++ Models/userTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace ST10372065.Models
+{
+    public class userTable : Controller
+    {
+        public static String con_String = "Server=tcp:cldvpart001-sql-server.database.windows.net,1433;Initial Catalog=cldvpart001-sql-DB;Persist Security Info=False;User ID=zack;Password={Teacupungold6};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        public static SqlConnection con = new SqlConnection(con_String);
+
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+
+        public int insert_User(userTable m)
+        {
+
+            try
+            {
+                string sql = "INSERT INTO userTable (userName, userSurname, userEmail) VALUES (@Name, @Surname, @Email)";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Name", m.Name);
+                cmd.Parameters.AddWithValue("@Surname", m.Surname);
+                cmd.Parameters.AddWithValue("@Email", m.Email);
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                con.Close();
+                return rowsAffected;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it appropriately
+                // For now, rethrow the exception
+                throw ex;
+            }
+
+
+        }
+
+    }
+}
