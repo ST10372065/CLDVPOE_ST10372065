3de5da35d7cc5c6eabafa7f439fd7626317ad5d8
Zack Stangroom <st10372065@vcconnect.edu.za>
2024-04-30 14:31:46 +0200

    created productTable and established connection

---
+++ Models/productTable.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace ST10372065.Models
+{
+    public class productTable : Controller
+    {
+        public static String con_String = "Server=tcp:cldvpart001-sql-server.database.windows.net,1433;Initial Catalog=cldvpart001-sql-DB;Persist Security Info=False;User ID=zack;Password={Teacupungold6};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        public static SqlConnection con = new SqlConnection(con_String);
+
+        public int ProductID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Availability { get; set; }
+
+
+
+        public int insert_product(productTable p)
+        {
+
+            try
+            {
+                string sql = "INSERT INTO productTable (productName, productPrice, productCategory, productAvailability) VALUES (@Name, @Price, @Category, @Availability)";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Name", p.Name);
+                cmd.Parameters.AddWithValue("@Price", p.Price);
+                cmd.Parameters.AddWithValue("@Category", p.Category);
+                cmd.Parameters.AddWithValue("@Availability", p.Availability);
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                con.Close();
+                return rowsAffected;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it appropriately
+                // For now, rethrow the exception
+                throw ex;
+            }
+
+
+        }
+
+
+        // Method to retrieve all products from the database
+        public static List<productTable> GetAllProducts()
+        {
+            List<productTable> products = new List<productTable>();
+
+            using (SqlConnection con = new SqlConnection(con_String))
+            {
+                string sql = "SELECT * FROM productTable";
+                SqlCommand cmd = new SqlCommand(sql, con);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    productTable product = new productTable();
+                    product.ProductID = Convert.ToInt32(rdr["productID"]);
+                    product.Name = rdr["productName"].ToString();
+                    product.Price = rdr["productPrice"].ToString();
+                    product.Category = rdr["productCategory"].ToString();
+                    product.Availability = rdr["productAvailability"].ToString();
+
+                    products.Add(product);
+                }
+            }
+
+            return products;
+        }
+    }
+}
