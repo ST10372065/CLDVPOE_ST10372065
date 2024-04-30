118ca1599e5e19780f6c416e040465464320673e
Zack Stangroom <st10372065@vcconnect.edu.za>
2024-04-30 16:28:24 +0200

    updated and added new models

---
+++ Models/LoginModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace ST10372065.Models
+{
+    public class LoginModel
+    {
+        public static String con_String = "Server=tcp:cldvpart001-sql-server.database.windows.net,1433;Initial Catalog=cldvpart001-sql-DB;Persist Security Info=False;User ID=zack;Password={Teacupungold6};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        public static SqlConnection con = new SqlConnection(con_String);
+
+        public int SelectUser(string email, string name)
+        {
+            int userId = -1; // Default value if user is not found
+            using (SqlConnection con = new SqlConnection(con_String))
+            {
+                string sql = "SELECT userID FROM userTable WHERE userEmail = @Email AND userName = @Name";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Name", name);
+                try
+                {
+                    con.Open();
+                    object result = cmd.ExecuteScalar();
+                    if (result != null && result != DBNull.Value)
+                    {
+                        userId = Convert.ToInt32(result);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Log the exception or handle it appropriately
+                    // For now, rethrow the exception
+                    throw ex;
+                }
+            }
+            return userId;
+        }
+    }
+}
--- Models/productTable.cs
+++ Models/productTable.cs
@@ -3,7 +3,7 @@ using System.Data.SqlClient;
 
 namespace ST10372065.Models
 {
-    public class productTable : Controller
+    public class productTable
     {
         public static String con_String = "Server=tcp:cldvpart001-sql-server.database.windows.net,1433;Initial Catalog=cldvpart001-sql-DB;Persist Security Info=False;User ID=zack;Password={Teacupungold6};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
         public static SqlConnection con = new SqlConnection(con_String);
